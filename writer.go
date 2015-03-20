56d0168167690d315ff2915a6838e71ba0f111ec
Seth Hoenig <seth.a.hoenig@gmail.com>
2015-03-19 21:17:35 -0500

    better documentation

--- writer.go
+++ writer.go
@@ -15,16 +15,20 @@ type CountingWriter struct {
 	written int
 }
 
+// NewCountingWriter wraps a Writer and keeps track of the number of
+// bytes that have been written to the Writer.
 func NewCountingWriter(w io.Writer) *CountingWriter {
 	return &CountingWriter{w, 0}
 }
 
+// Write writes bytes and returns the number of bytes written.
 func (w *CountingWriter) Write(p []byte) (n int, e error) {
 	n, e = w.writer.Write(p)
 	w.written += n
 	return n, e
 }
 
+// Written returns the number of bytes that have been written so far.
 func (w *CountingWriter) Written() int {
 	return w.written
 }
